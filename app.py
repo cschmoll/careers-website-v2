2c64dc18ffad6624e0c354318029bbd977d55925
cschmoll <69637555+cschmoll@users.noreply.github.com>
2023-12-01 16:37:12 +0000

    Several changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import json
+import json, random
 from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db
+from database import engine, load_jobs_from_db, load_job_from_db, Session
+from models import Base, Jobs
 
-#https://careers-website.cschmoll.repl.co/
+#https://careers-website-v2.cschmoll.repl.co/
 app = Flask(__name__)
 
 JOBS = [
@@ -35,18 +36,54 @@ JOBS = [
 @app.route("/")
 def hello_world():
 
-  jobs = load_jobs_from_db()
+  session = Session()
 
-  return render_template('home.html', jobs=jobs, company_name='Christof')
+  jobs = []
+  #jobs = load_jobs_from_db()
+  #jobs = session.query(Jobs).all()
+  for job in session.query(Jobs).all():
+    jobs.append(job.to_dict())
+
+  return render_template('home.html', jobs=jobs)
   #return render_template('home.html', jobs=JOBS, company_name='Christof' )
+
+
+@app.route("/job/<int:job_id>")
+def show_job(job_id):
+
+  #  jobs = load_jobs_from_db()
+
+  session = Session()
+
+  job = session.query(Jobs).filter(Jobs.id == job_id).first()
+  selected_job = job.to_dict()
+  #job = [job for job in jobs if job['id'] == job_id]
+  #if len(job) == 0:
+  if job is None:
+    return "Not Found", 404
+  else:
+    #job = job[0]
+
+    return render_template('job.html',
+                           job=selected_job)
 
 
 @app.route("/api/jobs")
 def list_jobs():
 
+  session = Session()
+
+  jobs = []
+
+  for job in session.query(Jobs).all():
+    jobs.append(job.to_dict())
+
   #jobs = load_jobs_from_db()
-  return jsonify(JOBS)
+  #return jsonify(JOBS)
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
+  #app.run(host='0.0.0.0', debug=True, port=random.randint(2000, 9000))
   app.run(host='0.0.0.0', debug=True)
+  #app.run(host='0.0.0.0', port=random.randint(2000, 9000))  # Dubugger
--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 import os
 
 engine = create_engine(os.environ['DB_CONNECTION_STRING'])
 
+Session = sessionmaker(bind=engine)
 
 def load_jobs_from_db():
   with engine.connect() as connection:
@@ -11,25 +13,34 @@ def load_jobs_from_db():
 
     jobs = []
     for element in result.all():
-      # Gib jedes Element mit der print-Funktion aus
+      jobs.append(element._mapping)
+
+    return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as connection:
+    result = connection.execute(text("select * from jobs where id = :val"), {'val': id})
+
+    jobs = []
+    for element in result.all():
       jobs.append(element._mapping)
 
     return jobs
 
 
-with engine.connect() as connection:
+#with engine.connect() as connection:
   # nur noch zu Dokuzwecken enthalten, wird nicht mehr genutzt
-  result = connection.execute(text("select * from jobs"))
-  print("Type result: ", type(result))
-  result_all = result.all()
-  print("Type result_all: ", type(result_all))
-  print("result_all: ", result_all)
-  for element in result_all:
+  #result = connection.execute(text("select * from jobs"))
+  #print("Type result: ", type(result))
+  #result_all = result.all()
+  #print("Type result_all: ", type(result_all))
+  #print("result_all: ", result_all)
+  #for element in result_all:
     # Gib jedes Element mit der print-Funktion aus
-    print("Type of element: ", type(element))
-    print("element.mapping :", element._mapping)
-    if 'title' in element._mapping:
-      print("Column 'title': %s" % element._mapping['title'])
-    for item in element._mapping.items():
-      print("item; ", item)
-      print("type of item: ", type(item))
+    #print("Type of element: ", type(element))
+    #print("element.mapping :", element._mapping)
+    #if 'title' in element._mapping:
+      #print("Column 'title': %s" % element._mapping['title'])
+    #for item in element._mapping.items():
+      #print("item; ", item)
+      #print("type of item: ", type(item))
